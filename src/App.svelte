609a5e55fe5cd31df6afa71588dc73fe08062d12
dev1week <rondo2860@gmail.com>
2026-02-18 15:47:34 +0900

    mend

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,42 @@
 <script>
-  import axios from 'axios';
+  import {Router, Route} from 'svelte-routing';
 
+  import Header from './components/common/Header.svelte';
+  import Footer from './components/common/Footer.svelte';
+  
+  import MainPage from './pages/MainPage.svelte';
+  import NowPage from './pages/NowPage.svelte';
+  import PopularPage from './pages/PopularPage.svelte';
+  import TopPage from './pages/TopPage.svelte'; 
+  import UpcomingPage from './pages/UpcomingPage.svelte';
 
-  const options = {
-    method: 'GET',
-    url: 'https://api.themoviedb.org/3/movie/now_playing',
-    params: {
-      language: 'ko',
-      page: 1,
-      api_key: '12f7f88dfa748cf62be62f1ae86e981f',
-    },
-    headers: {
-      accept: 'application/json',
-    },
-  };
+  import NowSubPage from './pages/NowSubPage.svelte';
+  import PopularSubPage from './pages/PopularSubPage.svelte';
+  import UpcomingSubPage from './pages/UpcomingSubPage.svelte';
+  import TopSubPage from './pages/TopSubPage.svelte';
 
-  let movies= [];
-
-  const getMovies = async () => {
-    try{
-      const response = await axios.request(options); 
-      movies = await response.data.results; 
-
-      console.log(movies); 
-      return response; 
-    }catch(error){
-      throw new Error(error); 
-    }
-  };
-
-  let promise = getMovies(); 
 </script>
 
 
+<svelte:head>
+  <title>OTT</title>
+</svelte:head>
 
-{#await promise}
-  <p> loading </p>
-{:then}
-  {#each movies as movie}
-    <article>
-      <h3> 영화명 : {movie.title}</h3>
-      <p> 설명 : {movie.overview}</p>
-      <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
-    </article>
-  {/each}
-{:catch}
-    <p> 에러가 발생하였습니다. </p>
-{/await}
+<Header/> 
+<Router>
+  <Route path="/" component={() => MainPage}/>
+
+  <Route path="/popular" component={() => PopularPage}/>
+  <Route path="/now" component={() => NowPage}/>
+  <Route path="/top" component={() => TopPage}/>
+  <Route path="/upcoming" component={() => UpcomingPage}/>
+
+  
+  <Route path="/popular/:id" component={() => PopularSubPage}/>
+  <Route path="/now/:id" component={() => NowSubPage}/>
+  <Route path="/top/:id" component={() => TopSubPage}/>
+  <Route path="/upcoming/:id" component={() => UpcomingSubPage}/>
+
+</Router>
+
+<Footer/> 
